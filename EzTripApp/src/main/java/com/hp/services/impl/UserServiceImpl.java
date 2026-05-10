e402ec11181549654011d5d63e0646604eda33b2
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-05-10 10:14:51 +0700

    fix: prepend 'ROLE_' to user role name for proper authorization

--- EzTripApp/src/main/java/com/hp/services/impl/UserServiceImpl.java
+++ EzTripApp/src/main/java/com/hp/services/impl/UserServiceImpl.java
@@ -69,7 +69,7 @@ public class UserServiceImpl implements UserService {
         }
 
         Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRoleId().getName()));
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRoleId().getName()));
 
         return new MyUserDetails(user.getId(), user.getPhoneNumber(), user.getPassword(), authorities);
     }
