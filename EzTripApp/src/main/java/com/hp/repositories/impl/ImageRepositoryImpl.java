5dbb9270303fac42863cca4825c3af2039d5e3e4
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-05-30 00:05:09 +0700

    feat: add service image delete feature

---
+++ EzTripApp/src/main/java/com/hp/repositories/impl/ImageRepositoryImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hp.repositories.impl;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hp.pojo.Image;
+import com.hp.repositories.ImageRepository;
+
+/**
+ *
+ * @author Joon
+ */
+@Repository
+@Transactional
+public class ImageRepositoryImpl implements ImageRepository {
+
+    @Autowired
+    private LocalSessionFactoryBean factory;
+
+    @Override
+    public Image getImageById(Integer id) {
+        Session s = this.factory.getObject().getCurrentSession();
+        return s.get(Image.class, id);
+    }
+
+    @Override
+    public void deleteImage(Image image) {
+        Session s = this.factory.getObject().getCurrentSession();
+        s.remove(image);
+    }
+
+}
